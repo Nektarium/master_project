eeb521ca5329af564bb9e7afd5dbf74c9bb6d8e6
Nektarium <shmorosoff@gmail.com>
2021-08-06 18:45:43 +0300

    Basic tests finished

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -20,10 +20,7 @@ class PostTest extends TestCase
     public function test_see1BlogPostWhenThereIs1()
     {
         // Arrange
-        $post = new BlogPost();
-        $post->title = 'New Title';
-        $post->content = 'Content of the BlogPost';
-        $post->save();
+        $post = $this->createDummyBlogPost();
 
         // Act
         $response = $this->get('/posts');
@@ -32,8 +29,8 @@ class PostTest extends TestCase
         $response->assertSeeText('New Title');
         
         $this->assertDatabaseHas('blog_posts', [
-            'title' => 'New Title',
-            'content' => 'Content of the BlogPost'
+            'title' => 'NEWER New Title',
+            'content' => 'NEWER Content of the BlogPost'
         ]); 
     }
 
@@ -66,5 +63,59 @@ class PostTest extends TestCase
 
         $this->assertEquals($messages['title'][0], 'The title must be at least 5 characters.');
         $this->assertEquals($messages['content'][0], 'The content must be at least 10 characters.');
+    }
+
+    public function testUpdateValid()
+    {
+        $post = $this->createDummyBlogPost();
+
+        // Works in Laravel 8
+        // $this->assertDatabaseHas('blog_posts', [
+        //     'title' => 'NEW New Title',
+        //     'content' => 'NEW Content of the BlogPost'
+        // ]); 
+            
+        // Another way for Laravel 7+ versions
+        $this->assertDatabaseHas('blog_posts', $post->getAttributes());
+
+        $params = [
+            'title' => 'A new named title',
+            'content' => 'Content was changed'
+        ];
+
+        $this->put("/posts/{$post->id}", $params)
+            ->assertStatus(302)
+            ->assertSessionHas('status');  
+        
+        $this->assertEquals(session('status'), 'The blog post was updated!');
+        $this->assertDatabaseMissing('blog_posts', $post->getAttributes());
+        $this->assertDatabaseHas ('blog_posts', [
+            'title' => 'A new named title',
+            'content' => 'Content was changed'
+        ]);
+    }
+
+    public function testDelete()
+    {
+        $post = $this->createDummyBlogPost();
+        $this->assertDatabaseHas('blog_posts', $post->getAttributes());
+
+        $this->delete("/posts/{$post->id}")
+            ->assertStatus(302)
+            ->assertSessionHas('status');
+
+        $this->assertEquals(session('status'), 'The blog post was deleted!');
+        $this->assertDatabaseMissing('blog_posts', $post->getAttributes()); 
+        
+    }
+
+    private function createDummyBlogPost(): BlogPost
+    {
+        $post = new BlogPost();
+        $post->title = 'NEWER New Title';
+        $post->content = 'NEWER Content of the BlogPost';
+        $post->save();
+
+        return $post;
     }
 }
